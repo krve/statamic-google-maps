50e426240ab444a63b8295e87e030b0bfafd1c71
Kristoffer Vestergaard <kristoffer@eyeforce.dk>
2016-07-29 15:32:24 +0200

    Added support for multiple on the same page

--- GoogleMaps/resources/views/partials/map.blade.php
+++ GoogleMaps/resources/views/partials/map.blade.php
@@ -1,28 +1,48 @@
-<div id="gmap" style="height:{{ $height }};width:{{ $width }};"></div>
+<div id="{{ $map_id }}" style="height:{{ $height }};width:{{ $width }};"></div>
 <script>
-    function initMap() {
-        var map = new google.maps.Map(document.getElementById('gmap'), {
-            zoom: {{ $zoom }},
+var maps = typeof maps === "undefined" ? [] : maps;
+
+maps.push({
+    map_id: "{{ $map_id }}",
+    zoom: {{ $zoom }},
+    address: "{{ $address }}"
+});
+
+function createNewGMAP() {
+    for (var i = 0; i < maps.length; i++) {
+        var element = document.getElementById(maps[i].map_id);
+        var map = new google.maps.Map(element, {
+            zoom: maps[i].zoom,
             center: {lat: -34.397, lng: 150.644}
         });
-        var geocoder = new google.maps.Geocoder();
 
-        geocodeAddress(geocoder, map);
+        geocodeAddress(map, maps[i].address);
     }
-    function geocodeAddress(geocoder, resultsMap) {
-        var address = "{!! $address !!}";
-        geocoder.geocode({'address': address}, function(results, status) {
-            if (status === 'OK') {
-                resultsMap.setCenter(results[0].geometry.location);
-                var marker = new google.maps.Marker({
-                    map: resultsMap,
-                    position: results[0].geometry.location
-                });
-            } else {
-                alert('Geocode was not successful for the following reason: ' + status);
-            }
-        });
-    }
+
+    maps = [];
+}
+function geocodeAddress(resultsMap, address) {
+    var geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+            });
+        } else {
+            console.log('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+var googleMapsAdded = typeof googleMapsAdded === "undefined" ? false : true;
+
+if (typeof google === 'undefined' && googleMapsAdded == false) {
+    var script = document.createElement('script');
+    script.src = "https://maps.googleapis.com/maps/api/js?key={{ $api_key }}&callback=createNewGMAP";
+    document.getElementsByTagName('head')[0].appendChild(script);
+    googleMapsAdded = true;
+}
 </script>
-<script src="https://maps.googleapis.com/maps/api/js?key={{ $api_key }}&callback=initMap"
-async defer></script>
