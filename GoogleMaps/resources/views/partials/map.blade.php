3edb66aba4ea0cbb71ce8edfe7c4e750a0cb843b
Kristoffer Vestergaard <kristoffer@eyeforce.dk>
2017-03-06 11:12:30 +0100

    Rewrote JS into a class
    I rewrote it because it was needed to allow for markers
    and multiple maps on the same page.

--- GoogleMaps/resources/views/partials/map.blade.php
+++ GoogleMaps/resources/views/partials/map.blade.php
@@ -1,48 +1,107 @@
 <div id="{{ $map_id }}" style="height:{{ $height }};width:{{ $width }};"></div>
 <script>
-var maps = typeof maps === "undefined" ? [] : maps;
+    if (typeof GoogleMap === 'undefined') {
+        var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-maps.push({
-    map_id: "{{ $map_id }}",
-    zoom: {{ $zoom }},
-    address: "{{ $address }}"
-});
+        function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function createNewGMAP() {
-    for (var i = 0; i < maps.length; i++) {
-        var element = document.getElementById(maps[i].map_id);
-        var map = new google.maps.Map(element, {
-            zoom: maps[i].zoom,
-            center: {lat: -34.397, lng: 150.644}
-        });
+        window.GoogleMap = function () {
+            function GoogleMap(id, options) {
+                _classCallCheck(this, GoogleMap);
 
-        geocodeAddress(map, maps[i].address);
+                this.id = id;
+                this.options = options || {};
+                this.markers = options.markers || [];
+                this.isCentered = false;
+
+                this.init();
+
+                for(var i = 0; i < this.markers.length; i++) {
+                    this.geocodeAddress(this.markers[i], function (location) {
+                        this.addMarker(location);
+                    }.bind(this));
+                }
+            }
+
+            _createClass(GoogleMap, [{
+                key: 'init',
+                value: function init() {
+                    this.element = document.getElementById(this.id);
+
+                    if (typeof this.element === 'undefined') {
+                        throw new Error('The element does not exist [#' + this.id + ']');
+                    }
+
+                    this.map = new google.maps.Map(this.element, {
+                        zoom: this.options.zoom,
+                        center: { lat: -34.397, lng: 150.644 }
+                    });
+
+                    if (typeof this.options.address !== 'undefined') {
+                        this.geocodeAddress(this.options.address, function (location) {
+                            if (this.isCentered == false) {
+                                this.map.setCenter(location);
+                                this.isCentered = true;
+                            }
+
+                            if (this.markers.length <= 0) {
+                                this.addMarker(location);
+                            }
+                        }.bind(this));
+                    }
+                }
+            }, {
+                key: 'geocodeAddress',
+                value: function geocodeAddress(address, callback) {
+                    var geocoder = new google.maps.Geocoder();
+
+                    geocoder.geocode({ address: address }, function (results, status) {
+                        if (status === 'OK') {
+                            callback(results[0].geometry.location);
+                        } else {
+                            throw new Error('Geocode was not successful for the following reason: ' + status);
+                        }
+                    }.bind(this));
+                }
+            }, {
+                key: 'addMarker',
+                value: function addMarker(location) {
+                    this.markers.push(new google.maps.Marker({
+                        map: this.map,
+                        position: location
+                    }));
+                }
+            }]);
+
+            return GoogleMap;
+        }();
+
+        var script = document.createElement('script');
+        script.src = "https://maps.googleapis.com/maps/api/js?key={{ $api_key }}&callback=GoogleMapInitialize";
+        document.getElementsByTagName('head')[0].appendChild(script);
+        googleMapsAdded = true;
     }
 
-    maps = [];
-}
-function geocodeAddress(resultsMap, address) {
-    var geocoder = new google.maps.Geocoder();
+    if (typeof window.googleMaps === 'undefined') {
+        window.googleMaps = [];
+    }
 
-    geocoder.geocode({'address': address}, function(results, status) {
-        if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-
-            var marker = new google.maps.Marker({
-                map: resultsMap,
-                position: results[0].geometry.location
-            });
-        } else {
-            console.log('Geocode was not successful for the following reason: ' + status);
-        }
+    window.googleMaps.push({
+        id: "{{ $map_id }}",
+        zoom: {{ $zoom}},
+        address: "{{ $address }}",
+        @if (count($markers) > 0)
+            markers: ["{!! implode('", "', $markers) !!}"]
+        @endif
     });
-}
-var googleMapsAdded = typeof googleMapsAdded === "undefined" ? false : true;
 
-if (typeof google === 'undefined' && googleMapsAdded == false) {
-    var script = document.createElement('script');
-    script.src = "https://maps.googleapis.com/maps/api/js?key={{ $api_key }}&callback=createNewGMAP";
-    document.getElementsByTagName('head')[0].appendChild(script);
-    googleMapsAdded = true;
-}
+    function GoogleMapInitialize() {
+        for(var i = 0; i < window.googleMaps.length; i++) {
+            new GoogleMap(window.googleMaps[i].id, {
+                zoom: window.googleMaps[i].zoom,
+                address: window.googleMaps[i].address,
+                markers: window.googleMaps[i].markers || [],
+            });
+        }
+    }
 </script>
